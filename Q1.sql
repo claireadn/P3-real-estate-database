fbf2cdd3f71a28ba1d3531f0a0a762f1e968a6c4
Claire Audran <114073039+claireadn@users.noreply.github.com>
2024-05-10 12:01:27 +0200

    Update Q1.sql

--- Q1.sql
+++ Q1.sql
@@ -171,3 +171,93 @@ WHERE
 GROUP BY c.nom_com
 HAVING nb_ventes >= 50
 ORDER BY nb_ventes DESC;
+
+# Query 10
+SELECT 
+    (SELECT 
+            ROUND(AVG(v.valeur_fonciere / b.surf_carrez),
+                        2)
+        FROM
+            vente AS v
+                JOIN
+            bien AS b ON v.id_bien = b.id_bien
+        WHERE
+            b.nb_pieces LIKE 2) AS prix_mcarrez_2,
+    (SELECT 
+            ROUND(AVG(v.valeur_fonciere / b.surf_carrez),
+                        2)
+        FROM
+            vente AS v
+                JOIN
+            bien AS b ON v.id_bien = b.id_bien
+        WHERE
+            b.nb_pieces LIKE 3) AS prix_mcarrez_3,
+    ROUND(((SELECT 
+                    ROUND(AVG(v.valeur_fonciere / b.surf_carrez),
+                                2)
+                FROM
+                    vente AS v
+                        JOIN
+                    bien AS b ON v.id_bien = b.id_bien
+                WHERE
+                    b.nb_pieces LIKE 2) - (SELECT 
+                    ROUND(AVG(v.valeur_fonciere / b.surf_carrez),
+                                2)
+                FROM
+                    vente AS v
+                        JOIN
+                    bien AS b ON v.id_bien = b.id_bien
+                WHERE
+                    b.nb_pieces LIKE 3)) / (SELECT 
+                    ROUND(AVG(v.valeur_fonciere / b.surf_carrez),
+                                2)
+                FROM
+                    vente AS v
+                        JOIN
+                    bien AS b ON v.id_bien = b.id_bien
+                WHERE
+                    b.nb_pieces LIKE 3) * 100.00,
+            2) AS diff_pourcentage;
+
+# Query 11
+WITH valeur_par_ville AS (
+SELECT 
+    d.code_dep, c.nom_com, AVG(v.valeur_fonciere) AS valeur
+FROM 
+    vente AS v
+JOIN 
+    bien AS b ON v.id_bien = b.id_bien
+JOIN 
+    commune AS c ON b.id_com = c.id_com
+JOIN 
+    departement AS d ON c.id_dep = d.id_dep
+WHERE 
+    d.code_dep IN ('06', 13, 33, 59, 69)
+GROUP BY d.code_dep, c.nom_com)
+SELECT 
+    code_dep AS "Département", nom_com AS "Commune", 
+    ROUND(valeur, 2) AS "Prix moyen"
+FROM 
+    (SELECT code_dep, nom_com, valeur,
+    rank() OVER (PARTITION 
+    BY code_dep 
+    ORDER BY valeur DESC) AS rang
+FROM valeur_par_ville) 
+    AS resultat 
+    WHERE rang <= 3;
+
+# Query 12
+SELECT 
+    c.nom_com,
+    c.pop_total,
+    ROUND(CONVERT(COUNT(b.id_bien), UNSIGNED) / c.pop_total * 1000,
+            2) AS nb_ventes_habt1000
+FROM
+    bien AS b
+        JOIN
+    commune AS c ON b.id_com = c.id_com
+WHERE
+    c.pop_total > 10000
+GROUP BY c.nom_com , c.pop_total
+ORDER BY nb_ventes_habt1000 DESC
+LIMIT 20;
