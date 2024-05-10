8484a9e99b41e170b2b1380769f128c1db80054c
Claire Audran <114073039+claireadn@users.noreply.github.com>
2024-05-10 11:55:21 +0200

    Update Q1.sql

--- Q1.sql
+++ Q1.sql
@@ -42,4 +42,21 @@ SELECT
 FROM
     bien AS b
 GROUP BY nb_pieces
+
+# Query 4
+SELECT 
+    d.nom_dep,
+    ROUND(AVG(v.valeur_fonciere / b.surf_carrez),
+            2) AS prix_mcarrez
+FROM
+    departement AS d
+        JOIN
+    commune AS c ON c.id_dep = d.id_dep
+        JOIN
+    bien AS b ON b.id_com = c.id_com
+        JOIN
+    vente AS v ON v.id_bien = b.id_bien
+GROUP BY d.nom_dep
+ORDER BY prix_mcarrez DESC
+LIMIT 10;
 ORDER BY proportion_ventes DESC;
