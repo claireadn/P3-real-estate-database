7c7a05614f79f93759cdb2bbcc06e0944965f6b8
Claire Audran <114073039+claireadn@users.noreply.github.com>
2024-05-10 12:02:01 +0200

    Update Q1.sql

--- Q1.sql
+++ Q1.sql
@@ -105,36 +105,16 @@ ORDER BY valeur_fonciere DESC
 LIMIT 10;
 
 # Query 7
-SELECT 
-    (SELECT 
-            COUNT(DISTINCT id_vente)
-        FROM
-            vente AS v
-        WHERE
-            v.date_mutation BETWEEN '2020-01-01' AND '2020-03-31') AS vente_trimestre_1,
-    (SELECT 
-            COUNT(DISTINCT id_vente)
-        FROM
-            vente AS v
-        WHERE
-            v.date_mutation BETWEEN '2020-04-01' AND '2020-06-30') AS vente_trimestre_2,
-    ROUND((((SELECT 
-                    COUNT(DISTINCT id_vente) AS vente_trimestre_2
-                FROM
-                    vente AS v
-                WHERE
-                    v.date_mutation BETWEEN '2020-04-01' AND '2020-06-30') - (SELECT 
-                    COUNT(DISTINCT id_vente) AS vente_trimestre_1
-                FROM
-                    vente AS v
-                WHERE
-                    v.date_mutation BETWEEN '2020-01-01' AND '2020-03-31')) / (SELECT 
-                    COUNT(DISTINCT id_vente) AS vente_trimestre_1
-                FROM
-                    vente AS v
-                WHERE
-                    v.date_mutation BETWEEN '2020-01-01' AND '2020-03-31') * 100),
-            2) AS evol_ventes;
+WITH vente_s1 AS (
+	SELECT ROUND(COUNT(id_vente),2) AS vente_trimestre_1
+	FROM vente AS v
+	WHERE v.date_mutation BETWEEN '2020-01-01' AND '2020-03-31'),
+vente_s2 AS (
+ SELECT ROUND(COUNT(id_vente),2) AS vente_trimestre_2
+ FROM vente AS v
+ WHERE v.date_mutation BETWEEN '2020-04-01' AND '2020-06-31')
+SELECT ROUND(((vente_trimestre_2 - vente_trimestre_1) / vente_trimestre_1 * 100), 2) AS evol_ventes
+FROM vente_s1, vente_s2;
 
 # Query 8
 SELECT 
