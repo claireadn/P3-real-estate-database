49234273f2ffd286df6b94cb768ebe7f804b3ca6
Claire Audran <114073039+claireadn@users.noreply.github.com>
2024-05-10 11:56:27 +0200

    Update Q1.sql

--- Q1.sql
+++ Q1.sql
@@ -103,3 +103,35 @@ WHERE
     code_type_local LIKE 2
 ORDER BY valeur_fonciere DESC
 LIMIT 10;
+
+# Query 7
+SELECT 
+    (SELECT 
+            COUNT(DISTINCT id_vente)
+        FROM
+            vente AS v
+        WHERE
+            v.date_mutation BETWEEN '2020-01-01' AND '2020-03-31') AS vente_trimestre_1,
+    (SELECT 
+            COUNT(DISTINCT id_vente)
+        FROM
+            vente AS v
+        WHERE
+            v.date_mutation BETWEEN '2020-04-01' AND '2020-06-30') AS vente_trimestre_2,
+    ROUND((((SELECT 
+                    COUNT(DISTINCT id_vente) AS vente_trimestre_2
+                FROM
+                    vente AS v
+                WHERE
+                    v.date_mutation BETWEEN '2020-04-01' AND '2020-06-30') - (SELECT 
+                    COUNT(DISTINCT id_vente) AS vente_trimestre_1
+                FROM
+                    vente AS v
+                WHERE
+                    v.date_mutation BETWEEN '2020-01-01' AND '2020-03-31')) / (SELECT 
+                    COUNT(DISTINCT id_vente) AS vente_trimestre_1
+                FROM
+                    vente AS v
+                WHERE
+                    v.date_mutation BETWEEN '2020-01-01' AND '2020-03-31') * 100),
+            2) AS evol_ventes;
